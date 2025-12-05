17b31e972cbb9c5473797424669710774004e0a7
Zefanya Ecklezia Saragih <zefanyaecklesia.s@gmail.com>
2025-12-05 08:53:09 +0700

    init

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": "/resources/js",
-            "@/components": "/resources/js/Components",
+            "@/components": "/resources/js/components",
             "@/lib": "/resources/js/lib",
         },
     },
